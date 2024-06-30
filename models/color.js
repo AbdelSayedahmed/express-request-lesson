84319407cde783daea1c8e541910fd9d65463e93
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-06-30 10:39:21 -0400

    udpdated colors.js file

--- models/color.js
+++ models/color.js
@@ -1,8 +1,8 @@
 module.exports = [
-  {id: 1, name: "MistyRose"},
-  {id: 2, name: "Coral"},
-  {id: 3, name: "Goldenrod"},
-  {id: 4, name: "MediumAquamarine"},
-  {id: 5, name: "DeepSkyBlue"},
-  {id: 6, name: "Violet"},
+  {id: 1, name: "MistyRose", isFavorite: false},
+  {id: 2, name: "Coral", isFavorite: false},
+  {id: 3, name: "Goldenrod", isFavorite: false},
+  {id: 4, name: "MediumAquamarine", isFavorite: false},
+  {id: 5, name: "DeepSkyBlue", isFavorite: false},
+  {id: 6, name: "Violet", isFavorite: false},
 ];
